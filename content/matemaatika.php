c9de59500f7c3c09cb8e28fbf489c5ce685eaccc
Irina Merkulova <irina.merkulova@tthk.ee>
2023-09-05 16:57:03 +0300

    php arvude kontroll

--- content/matemaatika.php
+++ content/matemaatika.php
@@ -3,11 +3,42 @@ echo "<h1>Matemaatika tehed</h1>";
 $number1=50;
 $number2=100;
 //Vastused -punase värviga ja lisada täpploend
-echo "Kui me liidame 2 arvu kokku, siis tuleb ".($number2 + $number1);
+//https://www.metshein.com/course/php-alused/curriculum/
+echo "Kui me liidame 2 arvu kokku, siis tuleb ";
+echo "<span>".($number2 + $number1)."</span>";
 echo "<br>";
-echo "Kui arv2 jagada arv1-ga, siis tuleb ".($number2 / $number1);
+echo "Kui arv2 jagada arv1-ga, siis tuleb";
+echo "<span>".($number2 / $number1)."</span>";
 echo "<br>";
 echo "Arv1 ruudus on ".pow($number1, 2);
 echo "<br>";
 echo "arv2-arv1 * 2 = ";
 echo ($number2-$number1 * 2);
+?>
+<h2>Kontrollimiseks sisesta 2 arvu</h2>
+<form name="kontroll" action="matemaatika.php">
+    <label for="n1">Arv 1</label>
+    <input type="number" name="n1">
+    <br>
+    <label for="n2">Arv 2</label>
+    <input type="number" name="n2" value="0">
+    <input type="submit" value="OK">
+</form>
+<?php
+if (isset($_REQUEST["n1"])) {
+
+    if ($_REQUEST["n1"] == $number1) {
+        if ($_REQUEST["n2"] == $number2) {
+            echo "Õige!";
+            echo "<body style='background-color: lightgreen'>";
+        } else if ($_REQUEST["n1"] !== $number1) {
+            echo "vale!";
+            echo "<body style='background-color: lightcoral'>";
+        } else {
+            echo "<body style='background-color: white'>";
+        }
+    } else {
+            echo "vale!";
+            echo "<body style='background-color: lightcoral'>";
+        }
+}
--- navigation.php
+++ navigation.php
@@ -1,7 +1,7 @@
 <nav>
     <ul>
         <li><a href="?">Avaleht</a></li>
-        <li><a href="http://merkulova.thkit.ee">HTML leht</a></li>
+        <li><a href="http://merkulova.thkit.ee/1kursus">HTML leht</a></li>
         <li><a href="?leht=joonis.php">JS Joonis</a></li>
         <li><a href="?leht=matemaatika.php">Matemaatika tehed</a></li>
         <li><a href="?">...</a></li>
